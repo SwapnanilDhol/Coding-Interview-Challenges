c2cd0f96b6b10424633d9891ea17779a4ddc0cb9
Swapnanil Dhol <swapnanildhol@gmail.com>
2020-04-30 02:45:41 +0530

    Update Remove-Element.cpp

--- CPP/Easy/Remove-Element.cpp
+++ CPP/Easy/Remove-Element.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-       int previous(0);
-       while (previous<nums.size()){
-           if (nums[previous]==val){
-               nums.erase( nums.begin( )+previous);
-           }
-           else previous+=1;
-           
-       }
-       return nums.size();
+    nums.erase(std::remove(nums.begin(), nums.end(), val),nums.end());
+    return nums.size();
     }
 };
